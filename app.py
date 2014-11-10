f77248e5d61b021cc89e8ab625a20f5e11d0cb57
alb-yeung <alb.yeung@gmail.com>
2014-11-10 13:13:50 -0500

    got it to pull from riot api

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import test
+
 app = Flask(__name__)
 
 @app.route("/",methods = ["GET","POST"])
@@ -10,8 +11,9 @@ def home():
     else:
         d = test.getSummonerData(summ)
         l = len(d["champions"])
-        return render_template("summonerdata.html",d=d,l=l,summ=summ)
+        every = test.getall()
+        return render_template("summonerdata.html",d=d,l=l,summ=summ,names=every)
 
 if __name__=="__main__":
-   app.debug=True
-   app.run(host="0.0.0.0",port=5000)
+    app.debug=True
+    app.run(host="0.0.0.0",port=5000)
