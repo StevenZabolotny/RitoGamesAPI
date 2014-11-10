9db44b96168e36bd79f08d0877b4c59b32bd97c2
StevenZabolotny <stevezabolotny@gmail.com>
2014-11-09 21:38:59 -0500

    Created a webpage that displays player data for a certain player.

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+import test
+app = Flask(__name__)
+
+@app.route("/",methods = ["GET","POST"])
+def home():
+    summ = request.form.get("summoner")
+    if (summ is None):
+        return render_template("home.html")
+    else:
+        d = test.getSummonerData(summ)
+        l = len(d["champions"])
+        return render_template("summonerdata.html",d=d,l=l,summ=summ)
+
+if __name__=="__main__":
+   app.debug=True
+   app.run(host="0.0.0.0",port=5000)
--- test.py
+++ test.py
@@ -1,24 +1,43 @@
 import urllib2
 import json
-url = """
-https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/NoNamer69?api_key=82bf0ad8-fcb0-4fe8-a911-ab05d5693242
-"""
 
-result=urllib2.urlopen(url)
-something = result.read()
-d = json.loads(something)
-for a in d:
-    summID = d[a]["id"]
+def convert(input):
+    if isinstance(input, dict):
+        return {convert(key): convert(value) for key, value in input.iteritems()}
+    elif isinstance(input, list):
+        return [convert(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
 
-#print summID
+def getSummonerData(summ):
+    url = """
+    https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/%s?api_key=82bf0ad8-fcb0-4fe8-a911-ab05d5693242
+    """
+    url = url%(summ)
+
+    result = urllib2.urlopen(url)
+    something = result.read()
+    d = json.loads(something)
+    for a in d:
+        summID = d[a]["id"]
+
+    #print summID
+
+    url = """
+    https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/%s/ranked?season=SEASON4&api_key=82bf0ad8-fcb0-4fe8-a911-ab05d5693242
+    """
+    url = url%(summID)
+
+    result = urllib2.urlopen(url)
+    something = result.read()
+    d = convert(json.loads(something))
+    #fixed = {}
+    #fixed["modifyDate"] = d[d.keys()[0]]
+    #fixed["summonerId"] = d[d.keys()[1]]
+    #fixed["champions"] = d[d.keys()[2]]
     
-url = """
-https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/%s/ranked?season=SEASON4&api_key=82bf0ad8-fcb0-4fe8-a911-ab05d5693242
-"""
-url = url%(summID)
+    print(d["champions"][0]["id"])
+    return d
 
-result=urllib2.urlopen(url)
-something = result.read()
-d = json.loads(something)
-for a in d:
-    print d[a]
